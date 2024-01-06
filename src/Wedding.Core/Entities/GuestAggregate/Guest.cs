4e7c394a19f81989df583580bf050810ef7ca45c
Freddy <freddymcgarry3@gmail.com>
2024-01-06 16:35:53 -0500

    renamed Attending to IsAttending

--- src/Wedding.Core/Entities/GuestAggregate/Guest.cs
+++ src/Wedding.Core/Entities/GuestAggregate/Guest.cs
@@ -7,6 +7,6 @@ public class Guest : IEntity
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public string? Email { get; set; }
-    public bool Attending { get; set; }
+    public bool IsAttending { get; set; }
     public FoodChoice DinnerSelection { get; set; }
 }
