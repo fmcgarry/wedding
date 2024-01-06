74c3c3e996484f096b8f71ee1ce7ce5fd08f8bdd
Freddy <freddymcgarry3@gmail.com>
2024-01-06 17:08:58 -0500

    added new guest command

---
+++ src/Wedding.UseCases/Guests/Queries/ListAttendingGuestsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Wedding.UseCases.Guests.Queries;
+
+public record ListAttendingGuestsQuery : IRequest<IEnumerable<GuestModel>>;
+
+public class ListAttendingGuestsHandler : IRequestHandler<ListAttendingGuestsQuery, IEnumerable<GuestModel>>
+{
+    public Task<IEnumerable<GuestModel>> Handle(ListAttendingGuestsQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
