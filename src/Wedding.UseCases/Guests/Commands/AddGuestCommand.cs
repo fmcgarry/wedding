2a4d17b180c3fe2cc63ed352637d03817d5f8c86
Freddy <freddymcgarry3@gmail.com>
2024-01-07 01:37:33 -0500

    removed logger from AddGuestCommand

--- src/Wedding.UseCases/Guests/Commands/AddGuestCommand.cs
+++ src/Wedding.UseCases/Guests/Commands/AddGuestCommand.cs
@@ -1,22 +1,19 @@
 ﻿using MediatR;
-using Microsoft.Extensions.Logging;
 using Wedding.Core.Entities.GuestAggregate;
 
 namespace Wedding.UseCases.Guests.Commands;
 
-public record AddGuestCommand(string Name, string Email) : IRequest<Guid>;
+public record AddGuestCommand(string Name, string? Email) : IRequest<Guid>;
 
-public class AddGuestHandler(ILogger<AddGuestCommand> _logger, IApplicationDbContext _dbContext) : IRequestHandler<AddGuestCommand, Guid>
+public class AddGuestHandler(IApplicationDbContext _dbContext) : IRequestHandler<AddGuestCommand, Guid>
 {
     public async Task<Guid> Handle(AddGuestCommand request, CancellationToken cancellationToken)
     {
-        _logger.LogTrace($"made it to {nameof(AddGuestCommand)}");
-
         var guestModel = new Guest()
         {
             Id = Guid.NewGuid(),
             Name = request.Name,
-            Email = request.Email,
+            Email = request.Email
         };
 
         await _dbContext.Guests.AddAsync(guestModel, cancellationToken);
