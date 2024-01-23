c90c036363955311696a28ea8d692189e09b0063
Freddy McGarry <fmcgarry@minitab.com>
2024-01-23 16:44:55 -0500

    populated SongRequestModel

--- src/Wedding.UseCases/SongRequests/SongRequestModel.cs
+++ src/Wedding.UseCases/SongRequests/SongRequestModel.cs
@@ -2,4 +2,9 @@
 
 public class SongRequestModel
 {
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public string? Artist { get; set; }
+    public Guid RequestedBy { get; set; }
+    public DateTime RequestedDate { get; set; }
 }
