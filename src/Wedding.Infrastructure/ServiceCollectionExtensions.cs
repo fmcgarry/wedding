30d106698674dc52d6eb1908637ee410f9e1bb0d
Freddy McGarry <fmcgarry@minitab.com>
2024-01-23 16:44:44 -0500

    fixed startup error caused from switching to photoclient

--- src/Wedding.Infrastructure/ServiceCollectionExtensions.cs
+++ src/Wedding.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Wedding.Core.Interfaces;
 using Wedding.Infrastructure.Data;
+using Wedding.Infrastructure.Photos;
 using Wedding.UseCases;
 
 namespace Wedding.Infrastructure;
@@ -15,6 +17,8 @@ public static class ServiceCollectionExtensions
             string? connectionString = configuration.GetConnectionString("ApplicationDb");
             options.UseSqlServer(connectionString);
         });
+
+        services.AddScoped<IPhotoClient, PhotoClient>();
 
         return services;
     }
