829ee9a44204bd74f2b927d20f13651c55bf8597
Freddy McGarry <fmcgarry@minitab.com>
2024-02-17 15:56:05 -0500

    added address to guest entity

---
+++ src/Wedding.Core/Entities/GuestAggregate/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Wedding.Core.Entities.GuestAggregate;
+public class Address
+{
+    public string? Line1 { get; set; }
+    public string? City { get; set; }
+    public StateTerritory? State { get; set; }
+    public string? Zip { get; set; }
+    public string? Phone { get; set; }
+}
--- src/Wedding.Core/Entities/GuestAggregate/Guest.cs
+++ src/Wedding.Core/Entities/GuestAggregate/Guest.cs
@@ -7,6 +7,7 @@ public class Guest : IEntity
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public string? Email { get; set; }
+    public Address? Address { get; set; }
     public bool IsAttending { get; set; }
     public DateTime? RsvpDate { get; set; }
     public FoodChoice? DinnerSelection { get; set; }
