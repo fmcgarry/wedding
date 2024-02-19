54919e1272c1165499d7407a8b4614215b5f25ed
Freddy <freddymcgarry3@gmail.com>
2024-02-18 22:51:02 -0500

    added quick facts, formatted guest table, and add guest button

--- src/Wedding.App/Components/Pages/ManageGuests/Guest.cs
+++ src/Wedding.App/Components/Pages/ManageGuests/Guest.cs
@@ -2,6 +2,17 @@
 
 public class Guest
 {
-    public string Name { get; set; }
-    public bool HasRsvpd { get; set; }
+    public enum RsvpState
+    {
+        Pending,
+        Attending,
+        Declined
+    }
+
+    public string Name { get; set; } = string.Empty;
+    public RsvpState Rsvp { get; set; }
+    public string InvitedBy { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Dinner { get; set; } = string.Empty;
 }
--- src/Wedding.App/Components/Pages/ManageGuests/ManageGuestsPage.razor.cs
+++ src/Wedding.App/Components/Pages/ManageGuests/ManageGuestsPage.razor.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.QuickGrid;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace Wedding.App.Components.Pages.ManageGuests;
@@ -6,11 +7,20 @@ public partial class ManageGuestsPage
 {
     private IQueryable<Guest>? _guests;
 
+    private PaginationState _guestsTablePagination = new() { ItemsPerPage = 10 };
+
     protected override void OnInitialized()
     {
         var list = new List<Guest>()
         {
-            new() {Name = "Taco McGinnen"}
+            new() { Name = "Taco McGinnen", Rsvp = Guest.RsvpState.Attending },
+            new() { Name = "Meatloaf Sunny", Rsvp = Guest.RsvpState.Attending, InvitedBy = "Ash Ketchum", Address = "123 Oak Street, Pallet Town, PA 12345", Phone = "814-867-5309", Dinner = "Meatloaf" },
+            new() { Name = "Tomato Souper" },
+            new() { Name = "Cheezy McGee" },
+            new() { Name = "Burrito Tyler", Rsvp = Guest.RsvpState.Attending },
+            new() { Name = "Sausage Smith" },
+            new() { Name = "Potato Tater", Rsvp = Guest.RsvpState.Declined },
+            new() { Name = "Ramen Namuri" },
         };
 
         _guests = list.AsQueryable();
@@ -18,16 +28,13 @@ public partial class ManageGuestsPage
 
     private void EditGuest(Guest guest)
     {
-
     }
 
     private void DeleteGuest(Guest guest)
     {
-
     }
 
     private void AddGuest(MouseEventArgs e)
     {
-
     }
 }
