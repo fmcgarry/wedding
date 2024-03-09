8405337cb3823a366a8a3fb089462e75bba96257
Freddy <freddymcgarry3@gmail.com>
2024-03-09 15:34:05 -0500

    ordered pages against a common structure

--- src/WeddingApp/Components/Pages/Gallery/GalleryPage.razor.cs
+++ src/WeddingApp/Components/Pages/Gallery/GalleryPage.razor.cs
@@ -1,6 +1,6 @@
-﻿namespace WeddingApp.Components.Pages;
+﻿namespace WeddingApp.Components.Pages.Gallery;
 
-public partial class Gallery
+public partial class GalleryPage
 {
     private List<string> _images = ["img/weddingcake.jpg", "img/weddingrings.png"];
     private string? _selectedImage;
