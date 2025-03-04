bc2546020636f25c547f526c014c94c4a3799c8f
Freddy <freddymcgarry3@gmail.com>
2025-03-04 11:48:03 -0500

    renamed WeddingSite2 to WeddingSite

--- src/WeddingSite2/Program.cs
+++ src/WeddingSite2/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using WeddingSite2;
+using WeddingSite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
